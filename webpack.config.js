5b2fa60ff214e86780ff70802723be8a5cedd4d1
atsuaa <sushidan.am618@gmail.com>
2022-02-20 17:03:27 +0900

    設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -8,10 +7,17 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "src/index.html",
-    }),
-  ],
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.common.dev",
+    },
+  },
 };
